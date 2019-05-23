8c65829726f53244a737209b4034877623806f19
ReedCJ <reedcj@my.lanecc.edu>
2019-05-22 23:33:50 -0700

    cannonfollow

--- 2D Robot Facility/Assets/Scripts/CannonFollow.cs
+++ 2D Robot Facility/Assets/Scripts/CannonFollow.cs
@@ -4,19 +4,25 @@ using UnityEngine;
 
 public class CannonFollow : MonoBehaviour
 {
-    private GameObject target;
-    private Vector3 targetPoint;
-    private Quaternion targetRotation;
+     private GameObject target;
+     private Vector3 targetPoint;
+     private Quaternion targetRotation;
+ 
+     void Start () 
+     {
+         target = GameObject.FindWithTag("Player");
+     }
+ 
+     void Update()
+     {
 
-    void Start()
-    {
-        target = GameObject.FindWithTag("Player");
-    }
-
-    void Update()
-    {
-        targetPoint = new Vector3(target.transform.position.x, transform.position.y, target.transform.position.z) - transform.position;
-        targetRotation = Quaternion.LookRotation(-targetPoint, Vector3.up);
-        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * 2.0f);
+        Vector3 pos = transform.InverseTransformPoint(target.transform.position);
+        pos.x = 0;
+        float angle = Vector3.Angle(Vector3.up, pos);
+        angle = pos.y < 0 ? -angle : angle;
+        transform.Rotate(Vector3.right, angle, Space.Self);
+        //         targetPoint = new Vector3(target.transform.position.x, transform.position.y, target.transform.position.z) - transform.position;
+        //         targetRotation = Quaternion.LookRotation (-targetPoint, Vector3.up);
+        //        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * 2.0f);
     }
 }
