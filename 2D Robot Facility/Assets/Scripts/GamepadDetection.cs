da5a1611c859001a0e117f35fa9a3591e5cd826f
ReedCJ <ReedCJ@my.lanecc.edu>
2019-06-13 14:48:46 -0700

    Tutorial UI framework

---
+++ 2D Robot Facility/Assets/Scripts/GamepadDetection.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GamepadDetection : MonoBehaviour
+{
+    private int Xbox_One_Controller = 0;
+    private int PS4_Controller = 0;
+
+    void Update()
+    {
+        string[] names = Input.GetJoystickNames();
+        for (int i = 0; i < names.Length; i++)
+        {
+           // print(names[i].Length);
+            if (names[i].Length == 19)
+            {
+                //print("PS4 CONTROLLER IS CONNECTED");
+                PS4_Controller = 1;
+                Xbox_One_Controller = 0;
+            }
+            if (names[i].Length == 33)
+            {
+                //print("XBOX ONE CONTROLLER IS CONNECTED");
+                //set a controller bool to true
+                PS4_Controller = 0;
+                Xbox_One_Controller = 1;
+
+            }
+        }
+
+
+        if (Xbox_One_Controller == 1)
+        {
+            //do something
+        }
+        else if (PS4_Controller == 1)
+        {
+            //do something
+        }
+        else
+        {
+            // there is no controllers
+        }
+    }
+}
